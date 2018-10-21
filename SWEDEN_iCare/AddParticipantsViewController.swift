ed365a12584dbe1d3808fb730cec53887859889f
Zheng Wei Lim <z.lim4@student.unimelb.edu.au>
2018-10-22 00:06:38 +1100

    added comment and removed print statements

--- SWEDEN_iCare/AddParticipantsViewController.swift
+++ SWEDEN_iCare/AddParticipantsViewController.swift
@@ -19,12 +19,11 @@ class AddParticipantsViewController: UIViewController, ParticipantsSelectionDele
     var currentUser = Auth.auth().currentUser
     var selectedParticipants:[User]!
     var databaseGroup = Database.database().reference().child("groups")
-    
-    @IBOutlet weak var groupName: UITextField!
     var groupNameController: MDCTextInputControllerOutlined?
-
+    @IBOutlet weak var groupName: UITextField!
     @IBOutlet weak var createGroup: UIButton!
     
+    // connecting button to the next segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "participantsContainer" {
             let vc = segue.destination as! ChatsParticipantsViewController
@@ -36,11 +35,11 @@ class AddParticipantsViewController: UIViewController, ParticipantsSelectionDele
         selectedParticipants = participants
     }
     
+    // createGroupButtonPressed post group name and group members to firebase
     @IBAction func createGroupButtonPressed(_ sender: UIButton) {
         let ref = databaseGroup.childByAutoId()
         let group = ["name": groupName.text, "creator": currentUser?.uid] as! [String : Any]
         ref.setValue(group)
-        
         for selected in selectedParticipants{
             let key = ref.childByAutoId().key
             ref.updateChildValues(["participants/\(key)": selected.uid as Any])
